ae992d622eb505596077bb531d0e8a7a25712ceb
dricaurte29 <dricaurte@unal.edu.co>
2023-07-28 23:18:27 -0500

    comentarios

--- game.py
+++ game.py
@@ -3,26 +3,30 @@ import numpy as np
 import pandas as pd
 from utils import generacion
 
-
+# Read initial matrix data from 'matriz.xlsx' file using Pandas
+# The matrix should contain 0s and 1s, where 0 represents a dead cell and 1 represents a living cell
+# You can either provide the initial matrix in 'matriz.xlsx' or use np.random.randint to start with a random matrix
 datos = pd.read_excel('matriz.xlsx', header=None)
-
 M = datos.values
 
+# Time interval between generations (in milliseconds)
 TIME = 100
 
-# Dimensiones de la ventana
+# Dimensions of the window
 WIDTH, HEIGHT = 500, 500
 
-# Tamaño de los píxeles en el pixel art
+# Size of the pixels in the pixel art
 PIXEL_SIZE = 5
 
-# Dimensiones de la matriz
+# Dimensions of the matrix
 MATRIX_WIDTH, MATRIX_HEIGHT = WIDTH // PIXEL_SIZE, HEIGHT // PIXEL_SIZE
 SIZE = MATRIX_HEIGHT
 
-#M = np.random.randint(2, size=(SIZE, SIZE))
+# Uncomment the line below to start with a random matrix instead of the one from 'matriz.xlsx'
+# M = np.random.randint(2, size=(SIZE, SIZE))
 
 def draw_pixel_art(screen, matrix):
+    # Draw the pixel art on the screen based on the matrix
     for y, row in enumerate(matrix):
         for x, pixel_value in enumerate(row):
             pixel_color = (214, 255, 76) if pixel_value == 1 else (0, 0, 0)
@@ -34,7 +38,9 @@ def main():
     pygame.display.set_caption("Conway's Game of Life")
     clock = pygame.time.Clock()
 
-    matrix = generacion(M,SIZE)
+    # Generate the initial matrix using the data from 'matriz.xlsx' or the random matrix (uncommented line)
+    matrix = generacion(M, SIZE)
+
     start_time = pygame.time.get_ticks()
     running = True
 
@@ -44,14 +50,14 @@ def main():
                 running = False
 
         current_time = pygame.time.get_ticks()
-        if current_time - start_time >= TIME:  # Cambiar cada 1000 milisegundos (1 segundo)
-            matrix = generacion(M,SIZE)
+        if current_time - start_time >= TIME:  # Change every 1000 milliseconds (1 second)
+            matrix = generacion(M, SIZE)
             start_time = current_time
 
-        screen.fill((0, 0, 0))  # Limpia la pantalla con color negro
-        draw_pixel_art(screen, matrix)  # Dibuja la matriz en la pantalla
-        pygame.display.flip()   # Actualiza la pantalla
-        clock.tick(30)          # Velocidad de la animación (cuadros por segundo)
+        screen.fill((0, 0, 0))  # Clear the screen with black color
+        draw_pixel_art(screen, matrix)  # Draw the matrix on the screen
+        pygame.display.flip()   # Update the screen
+        clock.tick(30)          # Animation speed (frames per second)
 
     pygame.quit()
 
